7bb190e1b5ac1aa27733a413b7c49a71e244b1ad
Tatiana Sugina <internetova@yandex.ru>
2023-08-28 14:37:20 +0300

    feat: added export log_writer

--- lib/surf_logger.dart
+++ lib/surf_logger.dart
@@ -14,4 +14,5 @@
 
 export 'package:surf_logger/src/logger.dart';
 export 'package:surf_logger/src/log_strategy.dart';
+export 'package:surf_logger/src/log_writer.dart';
 export 'package:surf_logger/src/simple_log_strategy.dart';
